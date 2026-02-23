fdcba1021ae2ac7bf76d656e1f33985b28b586f1
SAHABAT Admin <admin@sahabat.local>
2026-02-24 05:32:39 +0700

    feat(ui): add missing pagination links to letter history tables

--- resources/views/panitia/letters/index.blade.php
+++ resources/views/panitia/letters/index.blade.php
@@ -146,7 +146,11 @@
                 </table>
             </div>
             
-
+            @if($history->hasPages())
+                <div class="px-6 py-4 border-t border-slate-100 bg-slate-50">
+                    {{ $history->links() }}
+                </div>
+            @endif
         </div>
     </div>
     
